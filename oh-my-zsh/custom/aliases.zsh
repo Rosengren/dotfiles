4aef08eb3a909011630aa3576b79fa23dd89f601
Rosengren <kev.rosengren@gmail.com>
2015-08-25 13:55:07 -0400

    Added cosco.vim and tagbar

--- oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/custom/aliases.zsh
@@ -13,7 +13,15 @@ alias gb="git branch"
 alias go="git checkout"
 
 alias undo="reset --soft HEAD~1"
+alias git="/usr/local/Cellar/git/2.4.6/bin/git"
 
 # Oh My Zsh
 alias zshtheme="vim ~/.oh-my-zsh/custom/themes/simpleton.zsh-theme && source ~/.zshrc"
 alias zc="vim ~/.zshrc && source ~/.zshrc"
+
+# Grep
+# i = ignore case
+# n = line number
+# r = recursive search
+alias grep='grep -inr --color=auto --exclude-dir={.bzr,.cvs,.git,.hg,.svn}'
+
